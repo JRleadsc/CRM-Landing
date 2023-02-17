2b27bcd2cde6e11f7166b48981ac0ab03bf28ca3
amene sadeghi <amene.sadeghi92@gmail.com>
2023-02-17 14:00:05 +0330

    add video section in index page

--- js/script.js
+++ js/script.js
@@ -70,14 +70,6 @@ const accordionItemHeaders = document.querySelectorAll(".accordion-item-header")
 const accordionItem = document.querySelectorAll('.faq .accordion-item');
 accordionItemHeaders.forEach(accordionItemHeader => {
    accordionItemHeader.addEventListener("click", event => {
-    
-     // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
-    
-//     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
-//     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
-//        currentlyActiveAccordionItemHeader.classList.toggle("active");
-//        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-//      }
 
      accordionItemHeader.classList.toggle("active");
      const accordionItemBody = accordionItemHeader.nextElementSibling;
@@ -105,3 +97,4 @@ accordionItemHeaders.forEach(accordionItemHeader => {
     
    });
 });
+

b1bb3f24db73715e83fa70e4a88d2b86d5b14677
Mahdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-02-28 18:20:06 +0330

    feat: created Login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./login.html"],
+  content: [
+    "./login.html"
+  ],
   theme: {
     extend: {
-      neutral: '#2C2D2E',
-      light: '#9EA8B2',
-      primary: '#3E369D',
-      secondary: '#EC4EAE',
-      accent: '#43AEE6'
-    },
-    fontFamily: {
-      iran: ['IRANSans']
+      fontFamily: {
+        iran: ['IRANSans']
+      }
     }
   },
   plugins: [

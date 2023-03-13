11fcb6fabf8b47b948d06aad7db790abc416443a
Mahdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-03-13 14:40:15 +0330

    created request demo pages

---
+++ components/request/first.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="form-start flex flex-col justify-between">
+    <div>
+      <div class="text-[24px] text-[#2C2D2E] font-bold">
+        <h2>
+          رایگان شروع کنید
+        </h2>
+      </div>
+
+      <form action="" class="pt-[38px] ">
+        <div class="">
+          <label for="phone" class="form-label">
+            شماره همراه
+            <span>*</span>
+          </label>
+          <input id="phone" type="text" class="form-control" placeholder="+98 --- --- ---">
+        </div>
+        <div class="">
+          <label for="email" class="form-label">
+            ایمیل
+            <span>*</span>
+          </label>
+          <input id="email" type="email" class="form-control" placeholder="Sample@gmail.com">
+        </div>
+        <div class="">
+          <label for="company" class="form-label">
+            نام شرکت
+            <span>*</span>
+          </label>
+          <input id="company" type="text" class="form-control" placeholder="نام شرکت خود را وارد کنید">
+        </div>
+        <div class="pt-[34px]">
+          <NuxtLink to="/request-demo-step-2">
+            <button class="btn-md pink-btn">
+              ارسال درخواست
+            </button>
+          </NuxtLink>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const formData = ref(null)
+
+const submitHandler = () => {
+  console.log(formData.value)
+}
+</script>
--- pages/request-demo.vue
+++ pages/request-demo.vue
@@ -46,65 +46,7 @@
           </div>
         </div>
         <div class="col-xl-6 col-lg-6 flex justify-end">
-          <div class="form-start flex flex-col justify-between">
-            <div>
-              <div class="text-[24px] text-[#2C2D2E] font-bold">
-                <h2>
-                  رایگان شروع کنید
-                </h2>
-                <h2 class="d-none">
-                  تاییدیه ایمیل
-                </h2>
-              </div>
-
-              <form action="" class="d-none pt-[38px] ">
-                <div class="">
-                  <label for="phone" class="form-label">
-                    شماره همراه
-                    <span>*</span>
-                  </label>
-                  <input id="phone" type="text" class="form-control" placeholder="+98 --- --- ---">
-                </div>
-                <div class="">
-                  <label for="email" class="form-label">
-                    ایمیل
-                    <span>*</span>
-                  </label>
-                  <input id="email" type="email" class="form-control" placeholder="Sample@gmail.com">
-                </div>
-                <div class="">
-                  <label for="company" class="form-label">
-                    نام شرکت
-                    <span>*</span>
-                  </label>
-                  <input id="company" type="text" class="form-control" placeholder="نام شرکت خود را وارد کنید">
-                </div>
-              </form>
-              <div class="content-confirm text-base text-[#6D728E] lh-lg pt-[38px]">
-                <p>
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و
-                </p>
-                <p>
-                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و
-                </p>
-              </div>
-            </div>
-            <div class="pt-[34px]">
-              <NuxtLink to="/request-demo-step-2">
-                <button class="btn-md pink-btn">
-                  ارسال درخواست
-                </button>
-              </NuxtLink>
-              <div>
-                <p class="text-[18px] text-[#2C2D2E] pt-4">
-                  آیا ایمیل را دریافت نکرده اید ؟
-                  <NuxtLink to="/request-demo-step-2" class="primary">
-                    ارسال مجدد
-                  </NuxtLink>
-                </p>
-              </div>
-            </div>
-          </div>
+          <RequestFirst />
         </div>
       </div>
       <div class="row">

b18e7f061767cd8286556eeec6f9392d1ef57ffe
Mahdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-03-15 09:16:01 +0330

    feat: request Demo form submit

--- components/request/first.vue
+++ components/request/first.vue
@@ -7,45 +7,92 @@
         </h2>
       </div>
 
-      <form action="" class="pt-[38px] ">
-        <div class="">
-          <label for="phone" class="form-label">
-            شماره همراه
-            <span>*</span>
-          </label>
-          <input id="phone" type="text" class="form-control ltr" placeholder="0915_______">
-        </div>
-        <div class="">
-          <label for="email" class="form-label">
-            ایمیل
-            <span>*</span>
-          </label>
-          <input id="email" type="email" class="form-control ltr" placeholder="Sample@gmail.com">
-        </div>
-        <div class="">
-          <label for="company" class="form-label">
-            نام شرکت
-            <span>*</span>
-          </label>
-          <input id="company" type="text" class="form-control" placeholder="نام شرکت خود را وارد کنید">
-        </div>
+      <FormKit
+        id="login-form"
+        v-model="formData"
+        type="form"
+        :actions="false"
+        form-class="pt-[38px]"
+        message-class="hidden"
+        @submit="submitHandler"
+      >
+        <FormKit
+          type="text"
+          name="mobile"
+          label="شماره همراه *"
+          label-class="form-label"
+          input-class="form-control ltr"
+          placeholder="0915_______"
+          message-class="text-xs -mt-4 mr-5 mb-2 text-red-500"
+          validation="required|mobileNumber"
+          :validation-messages="{
+            required: 'شماره همراه را وارد کنید',
+            mobileNumber: 'شماره همراه وارد شده معتبر نیست'
+          }"
+        />
+
+        <FormKit
+          type="text"
+          name="email"
+          label="ایمیل *"
+          label-class="form-label"
+          input-class="form-control ltr"
+          placeholder="sample@gmail.com"
+          message-class="text-xs -mt-4 mr-5 mb-2 text-red-500"
+          validation="required|email"
+          :validation-messages="{
+            required: 'ایمیل را وارد کنید',
+            email: 'ایمیل وارد شده معتبر نیست'
+          }"
+        />
+
+        <FormKit
+          type="text"
+          name="company"
+          label="نام شرکت *"
+          label-class="form-label"
+          input-class="form-control"
+          placeholder="نام شرکت خود را وارد کنید"
+          message-class="text-xs -mt-4 mr-5 mb-2 text-red-500"
+          validation="required"
+          :validation-messages="{
+            required: 'نام شرکت را وارد کنید'
+          }"
+        />
         <div class="pt-[34px]">
-          <NuxtLink to="/auth/step-1">
-            <button class="btn-md pink-btn">
-              ارسال درخواست
-            </button>
-          </NuxtLink>
+          <div v-if="loading" class="btn-md pink-btn">
+            <SvgLoading class="h-8 w-auto text-white" />
+          </div>
+          <FormKit v-else type="submit" input-class="btn-md pink-btn">
+            <span class="text-white">ارسال درخواست</span>
+          </FormKit>
         </div>
-      </form>
+      </Formkit>
     </div>
   </div>
 </template>
 
 <script setup>
-const formData = ref(null)
+import { phoneNumberNormalizer, digitsFaToEn, digitsArToEn } from '@persian-tools/persian-tools'
+
+const formData = ref({
+  mobile: ''
+})
+const loading = ref(false)
 
 const submitHandler = () => {
-  console.log(formData.value)
-  useRouter().push('/auth/step-1')
+  loading.value = true
+  formData.value.mobile = digitsFaToEn(digitsArToEn(phoneNumberNormalizer(formData.value.mobile, '0')))
+  apiRequest('post', sendRequestDemo(), formData.value)
+    .then((res) => {
+      console.log(res)
+      useRouter().push('/auth/step-1')
+    })
+    .catch((e) => {
+      console.error(e)
+    })
+    .finally(() => {
+      loading.value = false
+    })
 }
 </script>

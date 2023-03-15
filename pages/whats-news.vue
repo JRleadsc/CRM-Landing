e3b6df782a88618bccd0829f68694c0c88f9f091
Mahdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-03-15 07:54:32 +0330

    created WhatsNews page

---
+++ pages/whats-news.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="mt-20">
+    <section>
+      <div class="container">
+        <div class="row pt-[33px]">
+          <div class="col-12">
+            <div>
+              <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item text-sm text-[#6D728E]">
+                    <NuxtLink to="/">
+                      صفحه اصلی
+                    </NuxtLink>
+                  </li>
+                  <li class="breadcrumb-item text-sm text-[#6D728E] active" aria-current="page">
+                    اخبار
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="whats-news pb-5">
+      <div class="container pb-5">
+        <div class="row text-center">
+          <div class="col-12">
+            <h1 class="heading_h">
+              اخبار
+            </h1>
+          </div>
+        </div>
+        <div class="row pb-3 mb-2">
+          <div class="col-lg-6 col-12">
+            <div class="news-list ltr">
+              <ul class="rtl scroll-custom">
+                <li class="d-flex align-center">
+                  <div class="date-news text-center">
+                    <span>
+                      مهر
+                    </span>
+                    <p>
+                      ۱۳۹۹
+                    </p>
+                  </div>
+                  <div class="poster-news">
+                    <img src="/images/news/n0.png" alt="">
+                  </div>
+                  <div class="title-news">
+                    <h3>
+                      مسیری به سمت مدیترانه
+                    </h3>
+                    <p>
+                      از سوی دیگر، ما با خشم عادلانه آن را محکوم می کنیم
+                    </p>
+                  </div>
+                </li>
+                <li class="d-flex align-center">
+                  <div class="date-news text-center">
+                    <span>
+                      مهر
+                    </span>
+                    <p>
+                      ۱۳۹۹
+                    </p>
+                  </div>
+                  <div class="poster-news">
+                    <img src="/images/news/n2.png" alt="">
+                  </div>
+                  <div class="title-news">
+                    <h3>
+                      مسیری به سمت مدیترانه
+                    </h3>
+                    <p>
+                      از سوی دیگر، ما با خشم عادلانه آن را محکوم می کنیم
+                    </p>
+                  </div>
+                </li>
+                <li class="d-flex align-center">
+                  <div class="date-news text-center">
+                    <span>
+                      مهر
+                    </span>
+                    <p>
+                      ۱۳۹۹
+                    </p>
+                  </div>
+                  <div class="poster-news">
+                    <img src="/images/news/n3.png" alt="">
+                  </div>
+                  <div class="title-news">
+                    <h3>
+                      مسیری به سمت مدیترانه
+                    </h3>
+                    <p>
+                      از سوی دیگر، ما با خشم عادلانه آن را محکوم می کنیم
+                    </p>
+                  </div>
+                </li>
+                <li class="d-flex align-center">
+                  <div class="date-news text-center">
+                    <span>
+                      مهر
+                    </span>
+                    <p>
+                      ۱۳۹۹
+                    </p>
+                  </div>
+                  <div class="poster-news">
+                    <img src="/images/news/n0.png" alt="">
+                  </div>
+                  <div class="title-news">
+                    <h3>
+                      مسیری به سمت مدیترانه
+                    </h3>
+                    <p>
+                      از سوی دیگر، ما با خشم عادلانه آن را محکوم می کنیم
+                    </p>
+                  </div>
+                </li>
+                <li class="d-flex align-center">
+                  <div class="date-news text-center">
+                    <span>
+                      مهر
+                    </span>
+                    <p>
+                      ۱۳۹۹
+                    </p>
+                  </div>
+                  <div class="poster-news">
+                    <img src="/images/news/n2.png" alt="">
+                  </div>
+                  <div class="title-news">
+                    <h3>
+                      مسیری به سمت مدیترانه
+                    </h3>
+                    <p>
+                      از سوی دیگر، ما با خشم عادلانه آن را محکوم می کنیم
+                    </p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6 d-lg-block d-none">
+            <div class="content-news ltr scroll-custom">
+              <div class="rtl">
+                <div class="poster-content">
+                  <img src="/images/news/n0.png" alt="image-news">
+                </div>
+                <div class="details-content">
+                  <h2>
+                    مسیری به سمت مدیترانه
+                  </h2>
+                  <p>
+                    از سوی دیگر، ما با خشم عادلانه و نفرت مردانی را محکوم می‌کنیم که از جذابیت‌های لذت لحظه‌ای چنان فریفته و تضعیف شده‌اند، آنچنان از هوس کور شده‌اند که نمی‌توانند درد را پیش‌بینی کنند…
+                  </p>
+                  <ul>
+                    <li>
+                      ردیابی مخاطبین و مدیریت خط لوله
+                    </li>
+                    <li>
+                      هر ایمیل تعامل مشتری، تماس ها، جلسات و موارد دیگر را ردیابی کنید
+                    </li>
+                    <li>
+                      دید کاملی از هر مشتری داشته باشید
+                    </li>
+                    <li>
+                      با گزارش‌ها و داشبوردها در یک نگاه، اطلاعات کسب‌وکار را دریافت کنید.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- <div class="row pt-5 mt-5 pb-5 mb-2">
+          <div class="col-12">
+            <div class="flex justify-center">
+              <button class="btn-md pink-btn">
+                ارسال درخواست
+              </button>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </section>
+  </div>
+</template>

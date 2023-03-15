fd45c1b0d55fb24284404711321772286bf97f7a
Mahdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-03-15 08:16:58 +0330

    created Help Center pages

--- components/layouts/default/header/nav.vue
+++ components/layouts/default/header/nav.vue
@@ -181,135 +181,9 @@
                 </NuxtLink>
               </li>
               <li>
-                <a class="dropdown-item" href="./help-center.html">
+                <NuxtLink to="/help-center" class="dropdown-item">
                   <div>
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="55" height="55" viewBox="0 0 89.768 85.354">
-                      <defs>
-                        <filter
-                          id="Rectangle_7966"
-                          x="0"
-                          y="9.643"
-                          width="75.212"
-                          height="75.711"
-                          filterUnits="userSpaceOnUse"
-                        >
-                          <feOffset dx="6" dy="9" input="SourceAlpha" />
-                          <feGaussianBlur stdDeviation="9" result="blur" />
-                          <feFlood flood-color="#45507a" flood-opacity="0.078" />
-                          <feComposite operator="in" in2="blur" />
-                          <feComposite in="SourceGraphic" />
-                        </filter>
-                        <filter
-                          id="Ellipse_576"
-                          x="10.753"
-                          y="0"
-                          width="76.451"
-                          height="76.381"
-                          filterUnits="userSpaceOnUse"
-                        >
-                          <feOffset dx="6" dy="9" input="SourceAlpha" />
-                          <feGaussianBlur stdDeviation="9" result="blur-2" />
-                          <feFlood flood-color="#45507a" flood-opacity="0.078" />
-                          <feComposite operator="in" in2="blur-2" />
-                          <feComposite in="SourceGraphic" />
-                        </filter>
-                        <filter
-                          id="Path_77784"
-                          x="26.675"
-                          y="17.504"
-                          width="63.093"
-                          height="63.069"
-                          filterUnits="userSpaceOnUse"
-                        >
-                          <feOffset dx="6" dy="9" input="SourceAlpha" />
-                          <feGaussianBlur stdDeviation="9" result="blur-3" />
-                          <feFlood flood-color="#45507a" flood-opacity="0.078" />
-                          <feComposite operator="in" in2="blur-3" />
-                          <feComposite in="SourceGraphic" />
-                        </filter>
-                      </defs>
-                      <g id="Group_31222" data-name="Group 31222" transform="translate(21 19.354)">
-                        <g transform="matrix(1, 0, 0, 1, -21, -19.35)" filter="url(#Rectangle_7966)">
-                          <g
-                            id="Rectangle_7966-2"
-                            data-name="Rectangle 7966"
-                            transform="translate(21 27.64)"
-                            fill="none"
-                            stroke="#40aae2"
-                            stroke-width="2.5"
-                          >
-                            <rect width="21.212" height="21.711" rx="7" stroke="none" />
-                            <rect
-                              x="1.25"
-                              y="1.25"
-                              width="18.712"
-                              height="19.211"
-                              rx="5.75"
-                              fill="none"
-                            />
-                          </g>
-                        </g>
-                        <path
-                          id="Path_77782"
-                          data-name="Path 77782"
-                          d="M-20197.922-15632.558h-7.309"
-                          transform="translate(20211.9 15648.046)"
-                          fill="none"
-                          stroke="#40a9e1"
-                          stroke-linecap="round"
-                          stroke-width="2.5"
-                        />
-                        <path
-                          id="Path_77783"
-                          data-name="Path 77783"
-                          d="M-20201.574-15632.558h-3.656"
-                          transform="translate(20215.477 15654.36)"
-                          fill="none"
-                          stroke="#40a9e1"
-                          stroke-linecap="round"
-                          stroke-width="2.5"
-                        />
-                        <g transform="matrix(1, 0, 0, 1, -21, -19.35)" filter="url(#Ellipse_576)" style="mix-blend-mode: multiply;isolation: isolate">
-                          <ellipse
-                            id="Ellipse_576-2"
-                            data-name="Ellipse 576"
-                            cx="9.115"
-                            cy="9.077"
-                            rx="9.115"
-                            ry="9.077"
-                            transform="matrix(-1, -0.09, 0.09, -1, 51.27, 39.03)"
-                            fill="none"
-                            stroke="#ec4eae"
-                            stroke-linecap="round"
-                            stroke-width="2.5"
-                          />
-                        </g>
-                        <g transform="matrix(1, 0, 0, 1, -21, -19.35)" filter="url(#Path_77784)">
-                          <path
-                            id="Path_77784-2"
-                            data-name="Path 77784"
-                            d="M5.557,5.534,0,0"
-                            transform="translate(55 42.81) rotate(180)"
-                            fill="none"
-                            stroke="#ec4eae"
-                            stroke-linecap="round"
-                            stroke-width="2.5"
-                          />
-                        </g>
-                        <path
-                          id="Path_77785"
-                          data-name="Path 77785"
-                          d="M0,0A4.589,4.589,0,0,0,4.574,3.53"
-                          transform="translate(26.7 9.741) rotate(-171)"
-                          fill="none"
-                          stroke="#e64cab"
-                          stroke-linecap="round"
-                          stroke-width="2.5"
-                          style="mix-blend-mode: multiply;isolation: isolate"
-                        />
-                      </g>
-                    </svg>
-
+                    <SvgFeatures />
                   </div>
                   <div>
                     <p>
@@ -319,7 +193,7 @@
                       در اینجا مقالات آموزشی وب سایت را ببینید
                     </span>
                   </div>
-                </a>
+                </NuxtLink>
               </li>
             </ul>
           </li>
---
+++ components/svg/search.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="22.074" height="22.872" viewBox="0 0 22.074 22.872">
+    <g transform="translate(0.074)">
+      <g transform="translate(0 0)">
+        <g fill="none" stroke="#fff" stroke-width="1.5">
+          <circle cx="11" cy="11" r="11" stroke="none" />
+          <circle cx="11" cy="11" r="10.25" fill="none" />
+        </g>
+        <path
+          d="M-4419.383-20760.232l-2.947,3.012"
+          transform="translate(4423.317 20779.031)"
+          fill="none"
+          stroke="#fff"
+          stroke-linecap="round"
+          stroke-width="1.5"
+        />
+      </g>
+    </g>
+  </svg>
+</template>

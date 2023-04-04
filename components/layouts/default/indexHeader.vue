9bdca2c0ae03e4041e9bb08c64731dccf7386da3
Mehdi Shah abbasian <mehdi.shahabbasian@gmail.com>
2023-04-04 09:52:34 +0430

    fix: lint

--- components/layouts/default/indexHeader.vue
+++ components/layouts/default/indexHeader.vue
@@ -1,16 +1,15 @@
 <template>
   <div class="relative">
- <header class="relative h-[80px] flex items-center px-md-0 px-3">
-   <div class="container">
-     <div class="row">
-       <div class="col-12 px-lg-3 px-0">
-         <LayoutsDefaultHeaderNav />
-       </div>
-     </div>
-   </div>
- </header>
- <div class="top-menu d-lg-none d-block absolute right-0 rounded-t-none z-10 bg-gradient-to-r from-[#EC4EAE] to-[#40AAE2]  mx-auto"></div>
- <div class="header-overlay"></div>
-</div>
+    <header class="relative h-[80px] flex items-center px-md-0 px-3">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 px-lg-3 px-0">
+            <LayoutsDefaultHeaderNav />
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="top-menu d-lg-none d-block absolute right-0 rounded-t-none z-10 bg-gradient-to-r from-[#EC4EAE] to-[#40AAE2]  mx-auto" />
+    <div class="header-overlay" />
+  </div>
 </template>
-
